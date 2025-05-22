049b52a3d85ca6946085fe50ec7e6b52f7c7df41
EvanPear <esp.builds@gmail.com>
2025-05-22 12:36:19 -0700

    feat: add hover-to-fill and update borders
    cells change color when dragged over with mouse button held down
    
    removed individual cell borders, added border to the container

--- main.js
+++ main.js
@@ -3,5 +3,22 @@ const gridContainer = document.querySelector('.grid-container');
 for(let i = 0; i < 256; i++) {
     const cell = document.createElement('div');
     cell.classList.add('cell');
+   
+   cell.addEventListener('mouseover', () => {
+    if (mouseDown) {
+        cell.style.backgroundColor = 'pink';
+    }
+   });
+   
     gridContainer.appendChild(cell);
 }
+
+let mouseDown = false;
+
+document.body.addEventListener('mousedown', () => {
+    mouseDown = true;
+});
+
+document.body.addEventListener('mouseup', () => {
+    mouseDown = false;
+});
